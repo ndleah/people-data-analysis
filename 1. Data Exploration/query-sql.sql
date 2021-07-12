49061520d69a08a45d400e8f8967908271561d16
Leah Nguyen <nduongthucanh@gmail.com>
2021-07-12 19:54:36 +0700

    Update query-sql.sql

--- 1. Data Exploration/query-sql.sql
+++ 1. Data Exploration/query-sql.sql
@@ -209,4 +209,89 @@ ORDER BY id;
 | d009  | Customer Service    |
 +───────+─────────────────────+
 
+--Update tables
 
+DROP SCHEMA IF EXISTS mv_employees CASCADE;
+CREATE SCHEMA mv_employees;
+
+-- department
+DROP MATERIALIZED VIEW IF EXISTS mv_employees.department;
+CREATE MATERIALIZED VIEW mv_employees.department AS
+SELECT * FROM employees.department;
+
+
+-- department employee
+DROP MATERIALIZED VIEW IF EXISTS mv_employees.department_employee;
+CREATE MATERIALIZED VIEW mv_employees.department_employee AS
+SELECT
+  employee_id,
+  department_id,
+  (from_date + interval '18 years')::DATE AS from_date,
+  CASE
+    WHEN to_date <> '9999-01-01' THEN (to_date + interval '18 years')::DATE
+    ELSE to_date
+    END AS to_date
+FROM employees.department_employee;
+
+-- department manager
+DROP MATERIALIZED VIEW IF EXISTS mv_employees.department_manager;
+CREATE MATERIALIZED VIEW mv_employees.department_manager AS
+SELECT
+  employee_id,
+  department_id,
+  (from_date + interval '18 years')::DATE AS from_date,
+  CASE
+    WHEN to_date <> '9999-01-01' THEN (to_date + interval '18 years')::DATE
+    ELSE to_date
+    END AS to_date
+FROM employees.department_manager;
+
+-- employee
+DROP MATERIALIZED VIEW IF EXISTS mv_employees.employee;
+CREATE MATERIALIZED VIEW mv_employees.employee AS
+SELECT
+  id,
+  (birth_date + interval '18 years')::DATE AS birth_date,
+  first_name,
+  last_name,
+  gender,
+  (hire_date + interval '18 years')::DATE AS hire_date
+FROM employees.employee;
+
+-- salary
+DROP MATERIALIZED VIEW IF EXISTS mv_employees.salary;
+CREATE MATERIALIZED VIEW mv_employees.salary AS
+SELECT
+  employee_id,
+  amount,
+  (from_date + interval '18 years')::DATE AS from_date,
+  CASE
+    WHEN to_date <> '9999-01-01' THEN (to_date + interval '18 years')::DATE
+    ELSE to_date
+    END AS to_date
+FROM employees.salary;
+
+-- title
+DROP MATERIALIZED VIEW IF EXISTS mv_employees.title;
+CREATE MATERIALIZED VIEW mv_employees.title AS
+SELECT
+  employee_id,
+  title,
+  (from_date + interval '18 years')::DATE AS from_date,
+  CASE
+    WHEN to_date <> '9999-01-01' THEN (to_date + interval '18 years')::DATE
+    ELSE to_date
+    END AS to_date
+FROM employees.title;
+
+-- Index Creation
+-- NOTE: we do not name the indexes as they will be given randomly upon creation!
+CREATE UNIQUE INDEX ON mv_employees.employee USING btree (id);
+CREATE UNIQUE INDEX ON mv_employees.department_employee USING btree (employee_id, department_id);
+CREATE INDEX        ON mv_employees.department_employee USING btree (department_id);
+CREATE UNIQUE INDEX ON mv_employees.department USING btree (id);
+CREATE UNIQUE INDEX ON mv_employees.department USING btree (dept_name);
+CREATE UNIQUE INDEX ON mv_employees.department_manager USING btree (employee_id, department_id);
+CREATE INDEX        ON mv_employees.department_manager USING btree (department_id);
+CREATE UNIQUE INDEX ON mv_employees.salary USING btree (employee_id, from_date);
+CREATE UNIQUE INDEX ON mv_employees.title USING btree (employee_id, title, from_date);
